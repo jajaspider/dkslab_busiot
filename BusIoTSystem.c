cae2d8f0e889c37c52790f2ef8dd8281c665df00
unknown <>
2017-03-18 23:31:57 +0900

    로그관리 추가

--- BusIoTSystem.c
+++ BusIoTSystem.c
@@ -20,6 +20,8 @@ char current_time[50];
 char logdata[100];
 int gps_x,gps_y,gps_time,temperature,humidity,passengercount,buttoncheck;
 char* trim(char *s);
+char* ltrim(char *s);
+char* rtrim(char* s);
 
 void log_management(char *log_data);
 int humidity_random_generation(int min,int max);
@@ -178,6 +180,43 @@ int setting_data(){
 char* trim(char *s) {
         return rtrim(ltrim(s));
 }
+
+// 문자열 우측 공백문자 삭제 함수
+char* rtrim(char* s) {
+  char t[MAX_STR_LEN];
+  char *end;
+
+  // Visual C 2003 이하에서는
+  // strcpy(t, s);
+  // 이렇게 해야 함
+  strcpy_s(t, s); // 이것은 Visual C 2005용
+  end = t + strlen(t) - 1;
+  while (end != t && isspace(*end))
+    end--;
+  *(end + 1) = '\0';
+  s = t;
+
+  return s;
+}
+
+
+// 문자열 좌측 공백문자 삭제 함수
+char* ltrim(char *s) {
+  char* begin;
+  begin = s;
+
+  while (*begin != '\0') {
+    if (isspace(*begin))
+      begin++;
+    else {
+      s = begin;
+      break;
+    }
+  }
+
+  return s;
+}
+
 
 int main(int argc,char *argv[])
 {
