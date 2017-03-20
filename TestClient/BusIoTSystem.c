7eba108bdd98dee30429d4a6ed73e9ad5efef3a9
unknown <>
2017-03-20 14:34:17 +0900

    주기 주가 및 분기 로그 추가

--- TestClient/BusIoTSystem.c
+++ TestClient/BusIoTSystem.c
@@ -194,7 +194,7 @@ int main(int argc,char *argv[])
 
                 timer = time(NULL);
                 t = localtime(&timer);
-                sprintf(current_day,"%d%d%d",t->tm_year+1900,t->tm_mon+1,t->tm_mday);
+                sprintf(current_day,"%d%02d%02d",t->tm_year+1900,t->tm_mon+1,t->tm_mday);
                 sprintf(log_time,"%d-%02d-%02d %02d:%02d:%02d",t->tm_year+1900,t->tm_mon+1,t->tm_mday,t->tm_hour,t->tm_min,t->tm_sec);
                 sprintf(current_time,"%02d%02d%02d",t->tm_hour,t->tm_min,t->tm_sec);
 
