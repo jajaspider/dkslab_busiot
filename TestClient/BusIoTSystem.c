0f261377e163caa0b54987b1c1a60448dbc318fa
jajaspider <jaja_spider@naver.com>
2017-03-22 17:05:04 +0900

    세팅값 변경에 따른 구조 변경

--- TestClient/BusIoTSystem.c
+++ TestClient/BusIoTSystem.c
@@ -200,14 +200,15 @@ int main(int argc,char *argv[])
                          */
 
                         //임시 세팅값 변경중
-                        sprintf(temp_string,"%%0%dx",settings[0].setting_data);
-                        sprintf(temp_string1,temp_string,current_time);
-
+                        // sprintf(temp_string,"%%0%dx",settings[0].setting_data);
+                        // sprintf(temp_string1,temp_string,current_time);
+                        sprintf(temp_string,"%%0%dx%%0%dx%%0%dx",settings[0].setting_data,settings[1].setting_data,settings[2].setting_data)
+                        sprintf(temp_string1,temp_string,t->tm_hour,t->tm_min,t->tm_sec);
                         printf("[DEBUG] current_time : %s\n", current_time);
 
                         strcat(buffer,temp_string1);
                         int j;
-                        for(j=1; j<setting_count; j+=1) {
+                        for(j=3; j<setting_count; j+=1) {
                                 sprintf(temp_string,"%%0%dx",settings[j].setting_data);
                                 sprintf(temp_string1,temp_string,random_generation(settings[j].setting_name,settings[j].min,settings[j].max));
                                 strcat(buffer,temp_string1);
