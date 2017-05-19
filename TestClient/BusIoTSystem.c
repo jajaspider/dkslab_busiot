f17add5ab447e46f0104529dba1d0aa6905f41c5
Sonagod <jaja_spider@naver.com>
2017-05-19 21:40:52 +0900

    BYTE 값으로 수정중

--- TestClient/BusIoTSystem.c
+++ TestClient/BusIoTSystem.c
@@ -276,21 +276,7 @@ int main(int argc,char *argv[])
                                         sprintf(print_str,"[BusIoTSystem] %s 값 추가 : %d%d%d",settings[i].setting_name,t->tm_hour,t->tm_min,t->tm_sec);
                                         printf("%s\n",print_str);
 
-                                }
-                                // 시간 시 분 초 밀리초 데이터
-                                else if(settings[j].min==9998&&settings[j].max==9998) {
-                                        sprintf(current_day,"%d%02d%02d",t->tm_year+1900,t->tm_mon+1,t->tm_mday);
-                                        sprintf(log_time,"%d-%02d-%02d %02d:%02d:%02d",t->tm_year+1900,t->tm_mon+1,t->tm_mday,t->tm_hour,t->tm_min,t->tm_sec);
-                                        sprintf(current_time,"%02d%02d%02d",t->tm_hour,t->tm_min,t->tm_sec);
-                                        sprintf(temp_string,"%%0%dd%%0%dd%%0%dd%%0%dd",(settings[j].setting_data)/4,(settings[j].setting_data)/4,(settings[j].setting_data)/4,((settings[j].setting_data)/4)*2);
-                                        //밀리초 데이터 가공
-                                        millisecond = (char*)&val.tv_usec;
-                                        millisecond = substring(millisecond,0,3);
-                                        sprintf(temp_string1,temp_string,t->tm_hour,t->tm_min,t->tm_sec,atoi(millisecond));
-                                        strcat(buffer,temp_string1);
-                                        sprintf(print_str,"[BusIoTSystem] %s 값 추가 : %d%d%d%d",settings[j].setting_name,(byte)t->tm_hour,(byte)t->tm_min,(byte)t->tm_sec,(byte)atoi(millisecond));
-                                        printf("%s\n",print_str);
-                                }
+                                }                              
                                 // STX 변환규칙
                                 else if(settings[j].min==9997&&settings[j].max==9997) {
                                         sprintf(temp_string1,(BYTE)0x02);
