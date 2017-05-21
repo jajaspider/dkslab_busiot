60bffcb6059033a1317ab99a568dc9393adbf7b5
Sonagod <jaja_spider@naver.com>
2017-05-21 19:55:24 +0900

    BYTE 수정중

--- TestClient/BusIoTSystem.c
+++ TestClient/BusIoTSystem.c
@@ -322,7 +322,7 @@ int main(int argc,char *argv[])
                         // printf("[BusIoTSystem] 데이터 총 길이 : %d\n",strlen(buffer));
                         // sprintf(logdata,"[BusIoTSystem] 데이터 총 길이 : %d",strlen(buffer));
                         // log_management(logdata);
-                        send(sock, g_sendBuff, data_count, 0);
+                        send(client_fd, g_sendBuff, data_count, 0);
 
                         printf("[BusIoTSystem] Send Data : %s\n",g_sendBuff);
                         sprintf(logdata,"BusIoTSystem : 전송된 데이터 : %s",g_sendBuff);
