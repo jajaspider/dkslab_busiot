a1e27aa58aaf202e1009f09e2386c62a561d21ee
jajaspider <jaja_spider@naver.com>
2017-06-08 00:08:52 +0900

    신 디자인 반영

--- UI2_Git/iotbus.js
+++ UI2_Git/iotbus.js
@@ -1,278 +1,165 @@
-$('#busTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-$('#listTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-$(document).ready(function() {
-    $("#passengerSlider").slider();
-    $("#temperatureSlider").slider();
-});
-
-
-$('#Disabled').modal()
-
-$('body').scrollspy({ target: '#menubar' })
-
-/*
-  var map;
-  function init() {
-    map = new daum.maps.Map(document.getElementById('map'), {
-      center: new daum.maps.LatLng(35.144809, 129.034790),
-      level: 5
-    });
-    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-  var mapTypeControl = new daum.maps.MapTypeControl();
-
-  // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-  // daum.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-  map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
-
-  // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-  var zoomControl = new daum.maps.ZoomControl();
-  map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
-
-  var imageSrc = 'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
-  imageSize = new daum.maps.Size(24, 29), // 마커이미지의 크기입니다
-  imageOption = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-  var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-  var clusterer = new daum.maps.MarkerClusterer({
-    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-    minLevel: 7 // 클러스터 할 최소 지도 레벨
-  });
-
-  $('#110-1').on('click', function() {
-    if ( $(this).prop('click') ) {
-      // 데이터를 가져오기 위해 jQuery를 사용합니다
-      // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
-      $.get("wherebus.json", function(data) {
-        // 데이터에서 좌표 값을 가지고 마커를 표시합니다
-        // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
-        var markers = $(data.positions).map(function(i, position) {
-          var maks =  new daum.maps.Marker({
-            map: map,
-            position : new daum.maps.LatLng(position.lat, position.lng),
-            image: markerImage,
-            clickable: true
-        });
-        maks.setMap(map);
-
-        var iwContent = '<span style="color: black">Hello World!</span>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-            iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-        var infowindow = new daum.maps.InfoWindow({
-          content : iwContent,
-          removable : iwRemoveable
-        });
-
-        daum.maps.event.addListener(maks, "click", function() {
-        infowindow.open(map, maks);
-        });
-
-        daum.maps.event.addListener(map, "click", function() {
-        infowindow.close();
-        });
-        return maks;
-      });
-
-        // 클러스터러에 마커들을 추가합니다
-        clusterer.addMarkers(markers);
-      });
-    } else {
-      clusterer.clear();
+var clickCount = 0;
+var passengerSliderValue;
+var temperatureSliderValue;
+var passengerVal;
+var mapContainer;
+var map;
+var clusterer;
+var marker;
+var infowindow;
+var markers = [];
+var passenger_num = new Array(23, 41, 88, 3, 62, 43, 55);
+var positions = [{
+        "lat": 35.147814,
+        "lng": 129.034641,
+        "store": '<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue0"></span>온도 : <span id="temperatureValue">31</span></div>'
+    },
+    {
+        "lat": 35.157818,
+        "lng": 129.058025,
+        "store": '<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue1"></span>온도 : <span id="temperatureValue">36</span></div>'
+    },
+    {
+        "lat": 35.163357,
+        "lng": 129.063777,
+        "store": '<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue2"></span>온도 : <span id="temperatureValue">18</span></div>'
+    },
+    {
+        "lat": 35.173202,
+        "lng": 129.071120,
+        "store": '<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue3"></span>온도 : <span id="temperatureValue">5</span></div>'
+    },
+    {
+        "lat": 35.186167,
+        "lng": 129.081328,
+        "store": '<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue4"></span>온도 : <span id="temperatureValue">11</span></div>'
+    },
+    {
+        "lat": 35.209991,
+        "lng": 129.078317,
+        "store": '<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue5"></span>온도 : <span id="temperatureValue">21</span></div>'
+    },
+    {
+        "lat": 35.155608,
+        "lng": 129.041998,
+        "store": '<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue6"></span>온도 : <span id="temperatureValue">32</span></div>'
     }
-  });
-  }
-
-*/
-
-  var clickCount = 0;
-  var passengerSliderValue;
-  var temperatureSliderValue;
-  var passengerVal;
-  var passenger_num = new Array(23, 41, 88, 3, 62, 43, 55);
-  var mapContainer;
-  var map;
-  var clusterer;
-  var markers = [];
-  var positions = [
-  {"lat":35.147814,"lng":129.034641,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue0"></span>온도 : <span id="temperatureValue">31</span></div>'},
-  {"lat":35.157818,"lng":129.058025,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue1"></span>온도 : <span id="temperatureValue">36</span></div>'},
-  {"lat":35.163357,"lng":129.063777,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue2"></span>온도 : <span id="temperatureValue">18</span></div>'},
-  {"lat":35.173202,"lng":129.071120,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue3"></span>온도 : <span id="temperatureValue">5</span></div>'},
-  {"lat":35.186167,"lng":129.081328,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue4"></span>온도 : <span id="temperatureValue">11</span></div>'},
-  {"lat":35.209991,"lng":129.078317,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue5"></span>온도 : <span id="temperatureValue">21</span></div>'},
-  {"lat":35.155608,"lng":129.041998,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue6"></span>온도 : <span id="temperatureValue">32</span></div>'},
-  {"lat":35.147825,"lng":129.034699,"store":
-    '<table style=\"text-align: center;\">'+
-    '<thead>'+
-    '<tr>'+
-    '<td>'+
-    '110-1 정보'+
-    '</td>'+
-    '</tr>'+
-    '</thead>'+
-    '<tbody>'+
-    '<tr>'+
-    '<td width="100" height="50">'+
-    '온도 : 50'+
-    '</td>'+
-    '<td width="100" height="50">'+
-    '습도 : 50'+
-    '</td>'+
-    '</tr>'+
-    '<tr>'+
-    '<td width="100" height="50">'+
-    '승객수 : 50'+
-    '</td>'+
-    '<td width="100" height="50">'+
-    '정보1 : 50'+
-    '</tr>'+
-    '<tr>'+
-    '</td>'+
-    '<td width="100" height="50">'+
-    '정보2 : 50'+
-    '<td width="100" height="50">'+
-    '정보3 : 50'+
-    '</td>'+
-    '</tr>'+
-    '</tbody>'+
-    '</table>'
-    }
-  ];
-  var marker;
-  var infowindow;
-  function init() {
-  mapContainer = document.getElementById('map'), // 지도를 표시할 div
-  mapOption = {
-      center: new daum.maps.LatLng(35.144809, 129.034790), // 지도의 중심좌표
-      level: 5 // 지도의 확대 레벨
-  };
-  map = new daum.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-  // 마커 클러스터러를 생성합니다
-  clusterer = new daum.maps.MarkerClusterer({
-      map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-      averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-      minLevel: 5 // 클러스터 할 최소 지도 레벨
-  });
-
-  //var markers = [];
-  /*
-  var positions = [
-  {"lat":35.147814,"lng":129.034641,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span name="passengerValue">23 </span>온도 : <span id="temperatureValue">31</span></div>'},
-  {"lat":35.157818,"lng":129.058025,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span name="passengerValue">41 </span>온도 : <span id="temperatureValue">36</span></div>'},
-  {"lat":35.163357,"lng":129.063777,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span name="passengerValue">88 </span>온도 : <span id="temperatureValue">18</span></div>'},
-  {"lat":35.173202,"lng":129.071120,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span name="passengerValue">3 </span>온도 : <span id="temperatureValue">5</span></div>'},
-  {"lat":35.186167,"lng":129.081328,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span name="passengerValue">62 </span>온도 : <span id="temperatureValue">11</span></div>'},
-  {"lat":35.209991,"lng":129.078317,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span name="passengerValue">43 </span>온도 : <span id="temperatureValue">21</span></div>'},
-  {"lat":35.155608,"lng":129.041998,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span name="passengerValue">55 </span>온도 : <span id="temperatureValue">32</span></div>'}
 ];
-*/
 
+function init() {
+    mapContainer = document.getElementById('map'), // 지도를 표시할 div
+        mapOption = {
+            center: new daum.maps.LatLng(35.144809, 129.034790), // 지도의 중심좌표
+            level: 5 // 지도의 확대 레벨
+        };
+    map = new daum.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-/*
-for(var i = 0; i< bus_num.length;i++)
-{
-  alert(bus_num[i]);
-}
-document.getElementById("#passengerValue0").innerHTML = bus_num[0];
-document.getElementById("#passengerValue2").innerHTML = bus_num[2];
-document.getElementById("#passengerValue1").innerHTML = bus_num[1];
-document.getElementById("#passengerValue3").innerHTML = bus_num[3];
-document.getElementById("#passengerValue4").innerHTML = bus_num[4];
-document.getElementById("#passengerValue5").innerHTML = bus_num[5];
-document.getElementById("#passengerValue6").innerHTML = bus_num[6];
-
-
-var positions = [
-{"lat":35.147814,"lng":129.034641,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue0"></span>온도 : <span id="temperatureValue">31</span></div>);'},
-{"lat":35.157818,"lng":129.058025,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue1"></span>온도 : <span id="temperatureValue">36</span></div>'},
-{"lat":35.163357,"lng":129.063777,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue2"></span>온도 : <span id="temperatureValue">18</span></div>'},
-{"lat":35.173202,"lng":129.071120,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue3"></span>온도 : <span id="temperatureValue">5</span></div>'},
-{"lat":35.186167,"lng":129.081328,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue4"></span>온도 : <span id="temperatureValue">11</span></div>'},
-{"lat":35.209991,"lng":129.078317,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue5"></span>온도 : <span id="temperatureValue">21</span></div>'},
-{"lat":35.155608,"lng":129.041998,"store":'<div>버스 번호 : <span id="bus_number">110-1 </span>승객 수 : <span id="passengerValue6"></span>온도 : <span id="temperatureValue">32</span></div>'}
-];*/
-  for (var i=0, len=positions.length; i<len; i++) {
-    markers.push(addMarker(positions[i]));
-   }
-
-   function addMarker(position) {
-     marker = new daum.maps.Marker({
-       position : new daum.maps.LatLng(position.lat, position.lng)
-     });
-
-    infowindow = new daum.maps.InfoWindow({
-      content: position.store // 인포윈도우에 표시할 내용
+    // 마커 클러스터러를 생성합니다
+    clusterer = new daum.maps.MarkerClusterer({
+        map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+        minLevel: 5 // 클러스터 할 최소 지도 레벨
     });
 
-      daum.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-      daum.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
-
-     return marker;
-   };
-   /*
-   //버스 승객 수 온도 확인
-   var passengerValueArray = document.getElementById("passengerValue");
-   var passengerVal;
-   for(var i in passengerValueArray)
-   {
-     passengerVal = passengerValueArray[i].value;
-   }
-   alert(passengerVal);
-   */
-
-  $('#110-1').click(function () {
-    if(clickCount == 0){
-    clusterer.addMarkers(markers);
-    clickCount = 1;
-
+    for (var i = 0, len = positions.length; i < len; i++) {
+        markers.push(addMarker(positions[i]));
     }
-    else{
-      clusterer.clear();
-      clickCount = 0;
+
+    function addMarker(position) {
+        marker = new daum.maps.Marker({
+            position: new daum.maps.LatLng(position.lat, position.lng)
+        });
+
+        infowindow = new daum.maps.InfoWindow({
+            content: position.store // 인포윈도우에 표시할 내용
+        });
+
+        daum.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+        daum.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+
+        return marker;
+    };
+
+    $('#110-1').click(function() {
+        if (clickCount == 0) {
+            clusterer.addMarkers(markers);
+            clickCount = 1;
+
+        } else {
+            clusterer.clear();
+            clickCount = 0;
+        }
+    })
+    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+    function makeOverListener(map, marker, infowindow) {
+        return function() {
+            infowindow.open(map, marker);
+            //승객 값
+
+            for (var i = 0; i < passenger_num.length; i++) {
+                $("#passengerValue" + i).text(passenger_num[i]);
+            }
+        };
     }
-  })
-  // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
-  function makeOverListener(map, marker, infowindow) {
-    return function() {
-      infowindow.open(map, marker);
-      //승객 값
 
-      for(var i =0;i<passenger_num.length;i++){
-      $("#passengerValue" + i).text(passenger_num[i]);
-      }
-    };
-  }
-
-  // 인포윈도우를 닫는 클로저를 만드는 함수입니다
-  function makeOutListener(infowindow) {
-    return function() {
-      infowindow.close();
-    };
-  }
+    // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+    function makeOutListener(infowindow) {
+        return function() {
+            infowindow.close();
+        };
+    }
 }
+
 function sensorSubmit() {
     //슬라이더 값
     passengerSliderValue = document.getElementById("passengerSlider").value;
     temperatureSliderValue = document.getElementById("temperatureSlider").value;
     clickCount = 1;
 
-  //  alert(passengerSliderValue);
-  //  alert(temperatureSliderValue);
-    for(var i =0; i<passenger_num.length;i++)
-    {
-      if(passenger_num[i] <= passengerSliderValue)
-      {
-
-        //markers[i].setMap(null); // 지도에서 제거한다.
-        //clusterer.removeMarkers( markers );
-        clusterer.removeMarker(markers[i]);
-      }
+    for (var i = 0; i < passenger_num.length; i++) {
+        if (passenger_num[i] <= passengerSliderValue) {
+            clusterer.removeMarker(markers[i]);
+        }
     }
+}
+
+
+$(document).ready(function() {
+    $(".search").keyup(function () {
+    var searchTerm = $(".search").val();
+    var listItem = $('.results tbody').children('tr');
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+
+    $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+      return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+    });
+
+    $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','false');
+    });
+
+    $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','true');
+    });
+
+    var jobCount = $('.results tbody tr[visible="true"]').length;
+    $('.counter').text(jobCount + ' item');
+
+    if(jobCount == '0') {$('.no-result').show();}
+    else {$('.no-result').hide();}
+        });
+  });
+
+function callBus(){
+var busTag = document.getElementById("Bus");
+
+var style = window.getComputedStyle(busTag);
+var v = style.getPropertyValue("visibility");
+
+if(v == "hidden")
+  busTag.style.visibility = "visible";
+else
+  busTag.style.visibility = "hidden";
+
 }
--- UI2_Git/main.php
+++ UI2_Git/main.php
@@ -70,17 +70,17 @@ else{
 
     <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="./bootstrap-slider.min.css" rel="stylesheet">
-    <link href="./iotbus.css" rel="stylesheet">
+    <link href="css/iotbus.css" rel="stylesheet">
     <!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
     <script src="./bootstrap/js/bootstrap.min.js"></script>
     <script src="./bootstrap-slider.min.js"></script>
-    <script src="./iotbus.js"></script>
-    <script type="text/javascript" src="http://apis.daum.net/maps/maps3.js?apikey=b3138577083cb1fe9cd9d4d6474e2a8e&libraries=services,clusterer,drawing"></script>
+    <script src="js/iotbus.js"></script>
+    <script type="text/javascript" src="http://apis.daum.net/maps/maps3.js?apikey=f06db548c98701cea74fdd012ba4bbec&libraries=services,clusterer,drawing"></script>
 
 
-    
+
 
   </head>
   <body onload="init()">
@@ -111,153 +111,103 @@ oci_execute($parse_b_bus);
 //$b_bus_carno = $row_b_bus['CARNO'];
 //$b_bus_busid = $row_b_bus['BUS_ID'];
 ?>
-    <!-- 목차 -->
-    <nav id="menubar" class="navbar navbar-inverse navbar-static-top">
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="/main.php#busPanel">Bus</a></li>
-          <li><a href="/main.php#sensorPanel">Sensor</a></li>
-          <li>
-            <a class="btn btn-default" href="#" role="button" data-toggle="modal" data-target="#Disabled" id="disabledButton">Panic</a>
-            <!-- Modal -->
-            <div class="modal fade" id="Disabled" tabindex="-1" role="dialog" aria-labelledby="DisabledLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="DisabledLabel">Panic</h4>
-                  </div>
-                  <div class="modal-body">
-                    Main text...
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <a class="btn btn-default" href="#" role="button" data-toggle="modal" data-target="#Disabled" id="disabledButton">Disabled Reservations</a>
-            <!-- Modal -->
-            <div class="modal fade" id="Disabled" tabindex="-1" role="dialog" aria-labelledby="DisabledLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="DisabledLabel">Disabled Reservations</h4>
-                  </div>
-                  <div class="modal-body">
-                    Main text...
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <!-- 머리말 -->
+<div id="map"></div>
     <header>
-      <div class="jumbotron">
-        <img id="imgStyle" src="./Img/Bus.png" class="img-responsive center-block" alt="Not Img" />
-        <p id="BIMS">
-            Bus Information Management System
-        </p>
-      </div>
+      <nav class="menu">
+       <input type="checkbox" href="#" class="menu-open" name="menu-open" id="menu-open" />
+       <label class="menu-open-button" for="menu-open">
+         <!--
+        <span class="lines line-1"></span>
+        <span class="lines line-2"></span>
+        <span class="lines line-3"></span>
+        -->
+      <img src="img/Bus.png" width="50%" height="50%">
+      </label>
+
+       <a href="javascript:void(0);" onclick="callBus()" class="menu-item blue"> 버스 </a>
+       <a href="javascript:void(0);" class="menu-item green"> 센서 </a>
+       <a href="javascript:void(0);" class="menu-item red"> <i class="fa fa-heart"></i> </a>
+       <a href="javascript:void(0);" class="menu-item purple"> <i class="fa fa-microphone"></i> </a>
+       <a href="javascript:void(0);" class="menu-item orange"> <i class="fa fa-star"></i> </a>
+       <a href="javascript:void(0);" class="menu-item lightblue"> <i class="fa fa-diamond"></i> </a>
+     </nav>
     </header>
 
-    <!-- 내용 -->
     <section>
-        <!-- 지도 -->
-        <div id="map"></div>
 
-        <!-- 검색 -->
-        <div id="formSearch">
-          <form class="form-inline">
-            <div class="form-group">
-              <label for="inputSearch" class="sr-only">Search</label>
-              <input type="search" class="form-control" id="inputSearch" placeholder="Search...">
-            </div>
-            <button type="reset" class="btn btn-default">Search</button>
-          </form>
+      <article>
+        <div id="Bus">
+          <div class="form-group pull-right">
+            <input type="text" class="search form-control" placeholder="Search..">
+          </div>
+          <table cellspacing='0' class="table table-hover table-bordered results"> <!-- cellspacing='0' is important, must stay -->
+          	<thead>
+          		<tr>
+          			<th>버스 번호</th>
+          			<th>출발 - 종착</th>
+          			<th>버스 종류</th>
+          		</tr>
+          	</thead><!-- Table Header -->
+
+          	<tbody>
+          		<tr>
+          			<td><a href"javascript:void(0);" id="110-1">110-1</a></td>
+          			<td>가야동 - 동래</td>
+          			<td>일반</td>
+          		</tr><!-- Table Row -->
+
+          		<tr>
+          			<td>11</td>
+          			<td>다대포 - 영도</td>
+          			<td>일반</td>
+          		</tr><!-- Darker Table Row -->
+
+          		<tr>
+          			<td>23</td>
+          			<td>용선 - 구포</td>
+          			<td>마을</td>
+          		</tr>
+
+          		<tr>
+          			<td>87</td>
+          			<td>토곡 - 까치고개</td>
+          			<td>일반</td>
+          		</tr>
+
+          		<tr>
+          			<td>115</td>
+          			<td>토곡 - 왕자맨션</td>
+          			<td>마을</td>
+          		</tr>
+
+          		<tr>
+          			<td>168</td>
+          			<td>용당 - 녹산동신호민원센터</td>
+          			<td>일반</td>
+          		</tr>
+
+          		<tr>
+          			<td>203</td>
+          			<td>죽전마을 - 온천장역</td>
+          			<td>좌석</td>
+          		</tr>
+
+          	</tbody>
+          </table>
         </div>
 
-        <!-- 버스 값들 -->
-        <div role="tabpanel" id="busPanel">
-          <ul class="nav nav-tabs nav-justified" role="tablist" id="busTab">
-            <li role="presentation" class="active"><a href="#busNum" aria-controls="busNum" role="tab" data-toggle="tab">Bus Number</a></li>
-            <li role="presentation"><a href="#busStop" aria-controls="busStop" role="tab" data-toggle="tab">Bus Stop</a></li>
-          </ul>
+      </article>
 
-            <div class="tab-content">
-            <div role="tabpanel" class="tab-pane fade in active" id="busNum">
-               <table>
-                    <tr><td><button id="110-1"type="button" class="btn btn-default">110-1</button></td></tr>
-                    <!--
-                    <?php
-                    /*
-                    while($row_b_bus = oci_fetch_array($parse_b_bus,OCI_ASSOC)){
-                       $busID = $row_b_bus['BUS_ID'];
-                      echo "<tr><td>버스ID: " . $busID . "</td></tr>";
-                      }
-                    */
-                      ?>-->
+      <article>
 
-            </table>
-            </div>
-            <div role="tabpanel" class="tab-pane fade" id="busStop">
-              <p>Bus Stop</p>
-            </div>
-          </div>
-
-        <!-- 센서 값들 -->
-        <div role="tabpanel" id="sensorPanel">
-          <ul class="nav nav-tabs nav-justified" role="tablist" id="listTab">
-            <li role="presentation" class="active"><a href="#Passenger" aria-controls="Passenger" role="tab" data-toggle="tab">Passenger</a></li>
-            <li role="presentation"><a href="#Temperature" aria-controls="Temperature" role="tab" data-toggle="tab">Temperature</a></li>
-            <li role="presentation"><a href="#State" aria-controls="State" role="tab" data-toggle="tab">State</a></li>
-          </ul>
-
-          <div class="tab-content">
-            <div role="tabpanel" class="tab-pane fade in active" id="Passenger">
-              <p>Passenger</p>
-              <div class='slider'>
-                  <input id="passengerSlider" type="text" class="span2" value="" data-slider-min="1" data-slider-max="100" data-slider-step="1" data-slider-value="[1,100]"/>
-              </div>
-            </div>
-            <div role="tabpanel" class="tab-pane fade" id="Temperature">
-              <p>Temperature</p>
-              <div class='slider'>
-                  <input id="temperatureSlider" type="text" class="span2" value="" data-slider-min="1" data-slider-max="100" data-slider-step="1" data-slider-value="[1,100]"/>
-              </div>
-            </div>
-            <div role="tabpanel" class="tab-pane fade" id="State">
-              <p>State</p>
-
-              <div class='slider'>
-                  <input id="statusSlider" type="text"
-                  data-provide="slider"
-                  data-slider-ticks="[1, 2, 3]"
-                  data-slider-ticks-labels='["Bad", "Normal", "Good"]'
-                  data-slider-min="1"
-                  data-slider-max="3"
-                  data-slider-step="1"
-                  data-slider-value="3"
-                  data-slider-tooltip="hide"
-                  />
-              </div>
-          </div>
-          </div>
-          <p><button type="button" class="btn btn-primary btn-block" id="sensorSubmitButton">Submit</button></p>
-        </div>
+      </article>
 
     </section>
-    <!-- 표 -->
-    <article>
 
-    </article>
-    <!-- 꼬리말 -->
     <footer>
 
     </footer>
 
   </body>
+
 </html>
