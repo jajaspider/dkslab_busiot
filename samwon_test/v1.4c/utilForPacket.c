6830800d0a16a9a2846aa905897a6ec26b22055c
jajaspider <jaja_spider@naver.com>
2017-06-26 15:00:50 +0900

    수정

--- samwon_test/v1.4c/utilForPacket.c
+++ samwon_test/v1.4c/utilForPacket.c
@@ -933,7 +933,7 @@ int32_t BakeBuffToSend (BYTE buff[SEND_BUFF_MAX_LENGTH], int32_t buffSize, BYTE 
 								}
 
 								_output_bakedbuff[length++] = _buff[buffSize-1];
-								_output_bakedbuff[336] = '\0';\
+								_output_bakedbuff[336] = '\0';
 								length=336;
 								return length;
 }
